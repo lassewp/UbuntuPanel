5f1bc115b1626cc50741b101fdc675c52126a981
Lasse Willaume Pedersen <14995@student.eadania.dk>
2022-03-19 20:24:05 +0100

    t

--- UbuntuPanel/infoWindow.xaml.cs
+++ UbuntuPanel/infoWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Renci.SshNet;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace UbuntuPanel
 {
@@ -19,9 +22,47 @@ namespace UbuntuPanel
     /// </summary>
     public partial class infoWindow : Window
     {
-        public infoWindow()
+        Server Server;
+        public infoWindow(Server server)
         {
             InitializeComponent();
+            Server = server;
+            TimeStart(0,0,2);
+            
+        }
+
+        private void TimeStart(int hours, int minutes, int seconds)
+        {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(hours, minutes, seconds);
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                var client = new SshClient(Server.ServerIP, Server.ServerPort, Server.ServerUsername, Server.ServerPassword);
+                try
+                {
+                    client.Connect();
+                }
+                catch (Exception)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        theViewbox.Items.Add("No connection...");
+                    });
+                }
+                var shell = client.CreateShellStream("Tail", 80, 24, 800, 600, 1024);
+
+                StreamWriter wr = new StreamWriter(shell);
+                StreamReader rd = new StreamReader(shell);
+                wr.AutoFlush = true;
+                wr.WriteLine("top");
+                string rep = shell.Read();
+            });
         }
     }
 }
